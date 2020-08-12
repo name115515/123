a71a3cdf93c6e220d90247d04b764f84c72302d8
name115515 <68314561+name115515@users.noreply.github.com>
2020-08-12 13:07:53 +0800

    Add files via upload

---
+++ 日期.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+current_date=datetime.now()
+print('今天是：'+str(current_date.year)+'年'+str(current_date.month)+'月'+str(current_date.day)+'日')
---
+++ 比较.py
@@ -0,0 +1,8 @@
+x = input('请输入一个数：')
+y = input('请输入要进行比较的另一个数：')
+if x == y:
+    print (str(x)+'等于'+str(y))
+elif x < y:
+    print (str(x)+'小于'+str(y))
+else :
+    print (str(x)+'大于'+str(y))
